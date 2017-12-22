845587f459e05e2805391b2fe58dae62b58a6c47
Travis CI User <travis@example.org>
2017-12-22 03:06:33 +0000

    Documentation update!

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['layer',['Layer',['../classTmx_1_1Layer.html#a6984126fbda7aecb74f4a124740e8672',1,'Tmx::Layer']]]
+  ['layer',['Layer',['../classTmx_1_1Layer.html#a6984126fbda7aecb74f4a124740e8672',1,'Tmx::Layer::Layer(const Tmx::Map *_map, const std::string _name, const int _x, const int _y, const int _width, const int _height, const float _opacity, const bool _visible, const LayerType _layerType)'],['../classTmx_1_1Layer.html#ab29120ce4695f1534209c4f1b4d981fa',1,'Tmx::Layer::Layer(const Tmx::Tile *_tile, const std::string _name, const int _x, const int _y, const int _width, const int _height, const float _opacity, const bool _visible, const LayerType _layerType)']]]
 ];
--- search/functions_9.js
+++ search/functions_9.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['objectgroup',['ObjectGroup',['../classTmx_1_1ObjectGroup.html#a99d89c89cd3262d19f4e30d6ba04667d',1,'Tmx::ObjectGroup']]],
+  ['objectgroup',['ObjectGroup',['../classTmx_1_1ObjectGroup.html#a99d89c89cd3262d19f4e30d6ba04667d',1,'Tmx::ObjectGroup::ObjectGroup(const Tmx::Map *_map)'],['../classTmx_1_1ObjectGroup.html#a0582d22f03335da57cd9785edc7121a0',1,'Tmx::ObjectGroup::ObjectGroup(const Tmx::Tile *_tile)']]],
   ['operator_21_3d',['operator!=',['../classTmx_1_1Color.html#a82c3e3199f93d1ce9c2babadd6a9bf40',1,'Tmx::Color']]],
   ['operator_3d',['operator=',['../classTmx_1_1Color.html#ac3b6cab5ff3052ff7253d70fc0ecba50',1,'Tmx::Color']]],
   ['operator_3d_3d',['operator==',['../classTmx_1_1Color.html#a7514ffb5992a35369655c2ca5e873012',1,'Tmx::Color']]]
